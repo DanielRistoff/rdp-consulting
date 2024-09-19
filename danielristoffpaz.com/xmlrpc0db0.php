54a43988f5ad8bcfba0f48df1148984e5c318a45
A0095582 <daniel.rpaz@telefonica.com>
2024-09-19 00:17:10 -0300

    adicionar arquivos do site

---
+++ danielristoffpaz.com/xmlrpc0db0.php
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<homePageLink>https://danielristoffpaz</homePageLink>
+		<apis>
+			<api name="WP-API" blogID="1" preferred="false" apiLink="http://127.0.0.1:5500/danielristoffpaz.com/drp-json/" />
+		</apis>
+	</service>
+</rsd>
+	
